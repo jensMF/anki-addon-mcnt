65691d674a9bbab9a52fcb2cc74c88f3c056938f
jensMF <jens.freudenthal@zoho.com>
2025-09-02 22:23:42 +0200

    added feature to reference answers on the back side

--- src/mcnt/__init__.py
+++ src/mcnt/__init__.py
@@ -1,196 +1,109 @@
-from aqt import Collection, fields, mw
-from aqt.gui_hooks import profile_did_open
+from aqt import Collection, mw
+from aqt.gui_hooks import profile_did_open, card_will_show
 from anki.consts import MODEL_STD
+from anki.cards import Card
 from functools import cache
 from typing import Any
 
-mcnt_model_name = "Multiple Choice Note Type"
-mcnt_card_name = "Multiple Choice Card"
+# --- Add-on Configuration ---
+mcnt_type_name = "Dynamic MCNT Type"
+mcnt_card_name = "Dynamic MCNT Card"
 
 front_template_path = "card-templates/front_template.html"
 back_template_path = "card-templates/back_template.html"
 styling_template_path = "card-templates/styling.css"
+script_path = "resources/script.js"
 
-# Get the folder path
+# --- Helper Functions ---
 def get_addon_path() -> str:
+    """Returns the full path to the add-on's folder."""
     return mw.addonManager.addonsFolder() + "/" + mw.addonManager.addonFromModule(__name__) + "/"
 
-# Load the template file to text
 @cache
-def load_template(path: str) -> str:
+def load_file(path: str) -> str:
+    """Loads a file from the add-on's directory."""
     with open(get_addon_path() + path, encoding="utf-8") as f:
         return f.read()
 
-def create_note_type(col: Collection) -> dict[str, Any]:
-    
-    print(f"Creating {mcnt_model_name} note type")
+# --- JavaScript Injection (The Anki-approved way) ---
 
-    model = col.models.new(mcnt_model_name)
-    model["type"] = MODEL_STD
-    model["sortf"] = 0
+def on_card_will_show(html: str, card: Card, context: str) -> str:
+    """
+    Appends the placeholder replacement script to the card's HTML when the answer is shown.
+    This is the correct way to add complex JS, as it avoids startup errors.
+    """
+    # Only run on the answer side in the reviewer, and only for our note type.
+    if context != "reviewAnswer" or card.note_type()["name"] != mcnt_type_name:
+        return html
 
-    # Add Fields
-    col.models.add_field(model, col.models.new_field("number"))
-    col.models.add_field(model, col.models.new_field("question"))
-    col.models.add_field(model, col.models.new_field("a"))
-    col.models.add_field(model, col.models.new_field("b"))
-    col.models.add_field(model, col.models.new_field("c"))
-    col.models.add_field(model, col.models.new_field("d"))
-    col.models.add_field(model, col.models.new_field("e"))
-    col.models.add_field(model, col.models.new_field("f"))
-    col.models.add_field(model, col.models.new_field("g"))
-    col.models.add_field(model, col.models.new_field("h"))
-    col.models.add_field(model, col.models.new_field("i"))
-    col.models.add_field(model, col.models.new_field("j"))
-    col.models.add_field(model, col.models.new_field("answers"))
-    col.models.add_field(model, col.models.new_field("ref"))
-    col.models.add_field(model, col.models.new_field("explanation"))
+    # Read the script from the dedicated .js file and wrap it in a <script> tag.
+    injected_script = f"<script>{load_file(script_path)}</script>"
+    return html + injected_script
 
-    # Create Card Template
-    template = col.models.new_template(mcnt_card_name)
-    col.models.add_template(model, template)
+# --- Note Type Creation and Management ---
 
-    if "isShuffle" in config:
-        if isinstance(config["isShuffle"], bool):
-            isShuffle = config["isShuffle"]
-        else:
-            isShuffle = False
-    else:
-        isShuffle = False
+def setup_note_type(col: Collection) -> None:
+    """
+    Creates or updates the MCNT note type with the correct fields, templates, and styling.
+    """
+    model = col.models.by_name(mcnt_type_name)
+    is_new = not model
 
-    if "isDisplayAnswerLetters" in config:
-        if isinstance(config["isDisplayAnswerLetters"], bool):
-            isDisplayAnswerLetters = config["isDisplayAnswerLetters"]
-        else:
-            isDisplayAnswerLetters = False
-    else:
-        isDisplayAnswerLetters = False
+    if is_new:
+        model = col.models.new(mcnt_type_name)
+        model["type"] = MODEL_STD
+        model["sortf"] = 0
 
-    if "isTTS" in config:
-        if isinstance(config["isTTS"], bool):
-            isTTS = config["isTTS"]
-        else:
-            isTTS = False
-    else:
-        isTTS = False
+        fields = ["number", "question", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "answers", "ref", "explanation"]
+        for f in fields:
+            col.models.add_field(model, col.models.new_field(f))
 
-    if "TTSLang" in config:
-        if isinstance(config["TTSLang"], str):
-            TTSLang = config["TTSLang"]
-        else:
-            TTSLang = "en_US"
-    else:
-        TTSLang = "en_US"
-    
-    front_template = load_template(front_template_path)
-    if isShuffle is True:
-        front_template = front_template.replace("isShuffle","true")
-    else:
-        front_template = front_template.replace("isShuffle","false")
-    if isDisplayAnswerLetters is True:
-        front_template = front_template.replace("isDisplayAnswerLetters","true")
-    else:
-        front_template = front_template.replace("isDisplayAnswerLetters","false")
-    if isTTS is False:
-        front_template = front_template.replace("<div id=\"tts\">{{tts TTSLang speed=0.8 voices=Apple_Samantha,Microsoft_Haruka:question}}</div>","<div id=\"tts\" style=\"width: 0px\"></div>")
-    front_template = front_template.replace("TTSLang",TTSLang)
+        template = col.models.new_template(mcnt_card_name)
+        col.models.add_template(model, template)
+
+    config = mw.addonManager.getConfig(__name__)
+    isShuffle = config.get("isShuffle", False)
+    isDisplayAnswerLetters = config.get("isDisplayAnswerLetters", True)
+    isTTS = config.get("isTTS", False)
+    TTSLang = config.get("TTSLang", "en_US")
+
+    # --- Configure Front Template ---
+    front_template = load_file(front_template_path)
+    front_template = front_template.replace("isShuffle", "true" if isShuffle else "false")
+    front_template = front_template.replace("isDisplayAnswerLetters", "true" if isDisplayAnswerLetters else "false")
+
+    tts_front_html = ""
+    if isTTS:
+        # The f-string requires doubling the braces to produce a single brace in the output.
+        tts_front_html = f'<div id="tts">{{{{tts {TTSLang} speed=0.8 voices=Apple_Samantha,Microsoft_Haruka:question}}}}</div>'
+    front_template = front_template.replace("<!-- TTS_FRONT -->", tts_front_html)
     model["tmpls"][0]["qfmt"] = front_template
 
-    back_template = load_template(back_template_path)
-    if isDisplayAnswerLetters is True:
-        back_template = back_template.replace("isDisplayAnswerLetters","true")
-    else:
-        back_template = back_template.replace("isDisplayAnswerLetters","false")
-    if isTTS is False:
-        back_template = back_template.replace("<legend id=\"tts\"><b>Explanation: {{tts TTSLang speed=0.8 voices=Apple_Samantha,Microsoft_Haruka:explanation}}</b></legend>","<legend id=\"tts\"><b>Explanation: </b></legend>")
-    back_template = back_template.replace("TTSLang",TTSLang)
+    # --- Configure Back Template ---
+    back_template = load_file(back_template_path)
+    back_template = back_template.replace("isDisplayAnswerLetters", "true" if isDisplayAnswerLetters else "false")
+
+    tts_back_html = "<b>Explanation: </b>"
+    if isTTS:
+        tts_back_html = f'<b>Explanation: {{{{tts {TTSLang} speed=0.8 voices=Apple_Samantha,Microsoft_Haruka:explanation}}}}</b>'
+    back_template = back_template.replace("<!-- TTS_BACK -->", tts_back_html)
     model["tmpls"][0]["afmt"] = back_template
 
-    styling = load_template(styling_template_path)
-    model["css"] = styling
+    # --- Configure Styling ---
+    model["css"] = load_file(styling_template_path)
 
-    col.models.add(model)
-   
-    return model
-
-def update_note_type(col: Collection) -> dict[str, Any]:
-    
-    model = mw.col.models.by_name(mcnt_model_name)
-
-    print(f"Updating {mcnt_model_name} note type")
-
-    # Update Card Template
-    if "isShuffle" in config:
-        if isinstance(config["isShuffle"], bool):
-            isShuffle = config["isShuffle"]
-        else:
-            isShuffle = False
+    # --- Save the model to the database ---
+    if is_new:
+        col.models.add(model)
     else:
-        isShuffle = False
+        col.models.save(model)
 
-    if "isDisplayAnswerLetters" in config:
-        if isinstance(config["isDisplayAnswerLetters"], bool):
-            isDisplayAnswerLetters = config["isDisplayAnswerLetters"]
-        else:
-            isDisplayAnswerLetters = False
-    else:
-        isDisplayAnswerLetters = False
+def on_profile_did_open() -> None:
+    """
+    A hook that runs when a user profile is loaded to ensure the note type exists and is up to date.
+    """
+    setup_note_type(mw.col)
 
-    if "isTTS" in config:
-        if isinstance(config["isTTS"], bool):
-            isTTS = config["isTTS"]
-        else:
-            isTTS = False
-    else:
-        isTTS = False
-
-    if "TTSLang" in config:
-        if isinstance(config["TTSLang"], str):
-            TTSLang = config["TTSLang"]
-        else:
-            TTSLang = "en_US"
-    else:
-        TTSLang = "en_US"
-    
-    front_template = load_template(front_template_path)
-    if isShuffle is True:
-        front_template = front_template.replace("isShuffle","true")
-    else:
-        front_template = front_template.replace("isShuffle","false")
-    if isDisplayAnswerLetters is True:
-        front_template = front_template.replace("isDisplayAnswerLetters","true")
-    else:
-        front_template = front_template.replace("isDisplayAnswerLetters","false")
-    if isTTS is False:
-        front_template = front_template.replace("<div id=\"tts\">{{tts TTSLang speed=0.8 voices=Apple_Samantha,Microsoft_Haruka:question}}</div>","<div id=\"tts\" style=\"width: 0px\"></div>")
-    front_template = front_template.replace("TTSLang",TTSLang)
-    model["tmpls"][0]["qfmt"] = front_template
-
-    back_template = load_template(back_template_path)
-    if isDisplayAnswerLetters is True:
-        back_template = back_template.replace("isDisplayAnswerLetters","true")
-    else:
-        back_template = back_template.replace("isDisplayAnswerLetters","false")
-    if isTTS is False:
-        back_template = back_template.replace("<legend id=\"tts\"><b>Explanation: {{tts TTSLang speed=0.8 voices=Apple_Samantha,Microsoft_Haruka:explanation}}</b></legend>","<legend id=\"tts\"><b>Explanation: </b></legend>")
-    back_template = back_template.replace("TTSLang",TTSLang)
-    model["tmpls"][0]["afmt"] = back_template
-
-    styling = load_template(styling_template_path)
-    model["css"] = styling
-
-    col.models.save(model)
-
-    return model
-
-def create_update_note_type():
-    model = mw.col.models.by_name(mcnt_model_name)
-
-    if not model:
-        create_note_type(mw.col)
-    else:
-        update_note_type(mw.col)
-
-config = mw.addonManager.getConfig(__name__)
-
-profile_did_open.append(create_update_note_type)
+# --- Register Hooks ---
+profile_did_open.append(on_profile_did_open)
+card_will_show.append(on_card_will_show)
